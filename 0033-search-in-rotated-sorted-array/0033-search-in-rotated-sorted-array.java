0cf7cb1d2e1a23318a1739fd08f72e6b00f57851
Akshay Varma <varmaakshay2020@gmail.com>
2026-07-15 23:00:21 +0530

    Time: 0 ms (100%), Space: 44.2 MB (11.2%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,43 +1,38 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length - 1;
-        
-        while (l <= r) {
-            int mid = l + (r - l) / 2; // Prevents integer overflow (safer than (l+r)/2)
-            
-            // Did we find it?
-            if (nums[mid] == target) {
+        int left = 0 ;
+        int right = nums.length - 1;
+
+        while (left<=right){
+            int mid = (left + right)/2;
+
+            if (nums[mid]==target){
                 return mid;
             }
+
+            // to check konsa halve is perfectly sorted 
+            if (nums[left]<=nums[mid]){
+             // left wale be bs lgao
+                if (target>=nums[left] && target<nums[mid]){
+                    right=mid-1;
+                }else {
+                    left = mid+1;
+                }
+
+
+            }else{
+             
+             
+                if (target<=nums[right] && target>nums[mid]){
+                    left=mid+1;
+                }else {
+                    right = mid-1;
+                }
+
             
-            // TRICK PART 1: Identify which half is perfectly sorted.
-            // If the left number is smaller than or equal to the mid number, 
-            // the left half is perfectly sorted.
-            if (nums[l] <= nums[mid]) {
-                
-                // TRICK PART 2: Check if our target lives inside this perfectly sorted left half.
-                if (target >= nums[l] && target < nums[mid]) {
-                    r = mid - 1; // It's in the left half, so discard the right.
-                } else {
-                    l = mid + 1; // It's NOT in the left half, so discard the left.
-                }
-                
-            } 
-            // Otherwise, the right half MUST be the perfectly sorted side.
-            else {
-                
-                // Check if our target lives inside this perfectly sorted right half.
-                if (target > nums[mid] && target <= nums[r]) {
-                    l = mid + 1; // It's in the right half, so discard the left.
-                } else {
-                    r = mid - 1; // It's NOT in the right half, so discard the right.
-                }
-                
             }
+            
         }
-        
-        // Target was not found
-        return -1; 
+        return -1;
     }
 }
